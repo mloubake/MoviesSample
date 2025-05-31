e1dce209330dee2bd485fc6ff78e7cc117e6db56
Matheus Loubake <mloubake@hotmail.com>
2025-05-31 18:38:30 -0300

    Creating a vertical recyclerView to group some Section's recyclerViews (horizontal).

---
+++ app/src/main/java/com/example/moviessample/Adapter/SectionsAdapter.kt
@@ -0,0 +1,19 @@
+package com.example.moviessample.Adapter
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.moviessample.ViewHolder.SectionsViewholder
+
+class SectionsAdapter(): RecyclerView.Adapter<SectionsViewholder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SectionsViewholder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: SectionsViewholder, position: Int) {
+        TODO("Not yet implemented")
+    }
+}
---
+++ app/src/main/java/com/example/moviessample/ViewHolder/SectionsViewholder.kt
@@ -0,0 +1,7 @@
+package com.example.moviessample.ViewHolder
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class SectionsViewholder(view: View): RecyclerView.ViewHolder(view) {
+}
